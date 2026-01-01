c50ec4a134d5fb9244705464df9ed7da46ccad58
jeffding <jeffding890430@gmail.com>
2026-01-01 04:30:41 +0000

    fix code

--- repo_opt/delete_modelscope.py
+++ repo_opt/delete_modelscope.py
@@ -14,24 +14,8 @@ listmodels=api.list_models(
     page_size=100
 )
 
-# 遍历 Models 列表，提取每个模型的 model_id
-model_ids = []
-for model in listmodels.get("Models", []):
-    backend_support = model.get("BackendSupport", {})
-    model_id = backend_support.get("model_id")
-    if model_id:
-        model_ids.append(model_id)
-
-# 输出提取到的 model_id
-print("提取到的 model_id 列表：")
-for idx, model_id in enumerate(model_ids, 1):
-    print(f"{idx}. {model_id}")
-
-print("输出过滤后的：")
-
-# 遍历模型并过滤
 for model in listmodels["Models"]:
-    delete_model_id = model["BackendSupport"]["model_id"]
-    if "prefix" in delete_model_id:
-        print(f"Deleting: {delete_model_id }")
-        api.delete_model(model_id=delete_model_id )
+    model_id = model["BackendSupport"]["model_id"]
+    if "prefix" in model_id:
+        print(f"Deleting: {model_id }")
+        api.delete_model(model_id=model_id )
